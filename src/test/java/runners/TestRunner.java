352044676b952d17678b305f1c9b2eb5357b2ff6
Riaz11-coder <64184169+Riaz11-coder@users.noreply.github.com>
2024-09-03 20:10:55 -0400

    update

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -14,7 +14,7 @@ import org.junit.runner.RunWith;
                 "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
         monochrome = true,
         dryRun = false,
-        tags = "@DynamicSearch"
+        tags = "@Login"
 )
 public class TestRunner {
 }
